009dbd48693a76054be79dfdefd17811be196a2c
Topceon1 <Topc_danil@mail.ru>
2022-02-02 19:26:11 +0600

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+"""главный файл программы
+ нейросеть для поиска лучшего варианта болида на сайте https://www.codingame.com/ в игре 'Mad Pod Racing'
+ """
+
+
+def generate_best_group(size_of_group: int):
+    for i in range(size_of_group):
+        print(i)
+
+
+if __name__ == '__main__':
+    generate_best_group(1000)
